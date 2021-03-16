54c461d20c7012a05c9038363d3d99fbdbf47a00
RezaCahyo01 <rezacw01@gmail.com>
2021-03-16 21:27:01 +0700

    PSJ-RezaCahyo

---
+++ check_host.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+
+if(len(sys.argv) <= 1):
+    print("IP address belum diberikan")
+else:
+    host_ip = sys.argv[1]
+    status, result = subprocess.getstatusoutput("ping -c1 " + host_ip)
+    if(status == 0):
+        print(f'Host {host_ip} is UP')
+    else:
+        print(f'Host {host_ip} is DOWN')
